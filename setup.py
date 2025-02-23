844c1c6bcbf25ad4c0428fb070e6d0fe0448eece
KH <karolagpe@hotmail.com>
2025-02-22 21:35:04 -0600

    Mi primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Karina Herrera",                         # Tu nombre
+    author_email="karina.herrera@iisi.com.mx",                 # Tu correo electrónico
+    url="https://github.com/karyHerrera/TercerRepo",     # URL del proyecto
+)
